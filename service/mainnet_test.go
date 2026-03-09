d3143f22a59a539745d857052a7d9f404a6116ef
houshuo.hhss <houshuo.hhss@bytedance.com>
2026-03-09 17:32:03 +0800

    feat update

--- service/mainnet_test.go
+++ service/mainnet_test.go
@@ -12,6 +12,14 @@ func TestETHBalance(t *testing.T) {
 	client.Init()
 	ctx := context.Background()
 
+	t.Run("TransferETH", func(t *testing.T) {
+		err := TransferETH(client.Client, consts.OWNER_PRIVATE_KEY, consts.USER_ADDRESS, 3)
+		if err != nil {
+			t.Logf("TransferETH failed, err: %v", err)
+			return
+		}
+	})
+
 	t.Run("GenerateNewAddress", func(t *testing.T) {
 		privateKey, publicKey, address, err := GenerateNewAddress()
 		if err != nil {
