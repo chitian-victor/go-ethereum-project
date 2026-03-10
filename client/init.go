7b90eb2750a5cc69e9cb85cd70ad57ea92a47669
houshuo.hhss <houshuo.hhss@bytedance.com>
2026-03-10 12:27:38 +0800

    feat update

--- client/init.go
+++ client/init.go
@@ -1,26 +1,27 @@
 package client
 
 import (
-	"github.com/ethereum/go-ethereum/ethclient"
+    "github.com/ethereum/go-ethereum/ethclient"
 )
 
 var (
-	Client    *ethclient.Client
-	SubClient *ethclient.Client
+    Client    *ethclient.Client
+    SubClient *ethclient.Client
 )
 
 func Init() {
-	// 因为都是 foundry 本地节点，接口调用默认明文传输不加密，所以都是 http 和 ws
+    // 因为都是 foundry 本地节点，接口调用默认明文传输不加密，所以都是 http 和 ws
+    // 另外foundry的 http 和 ws 都是一样的端口，其他服务器不一定的
 
-	blockClient, err := ethclient.Dial("http://localhost:8545")
-	if err != nil {
-		panic(err)
-	}
-	subBlockClient, err := ethclient.Dial("ws://localhost:8545")
-	if err != nil {
-		panic(err)
-	}
+    blockClient, err := ethclient.Dial("http://localhost:8545")
+    if err != nil {
+        panic(err)
+    }
+    subBlockClient, err := ethclient.Dial("ws://localhost:8545")
+    if err != nil {
+        panic(err)
+    }
 
-	Client = blockClient
-	SubClient = subBlockClient
+    Client = blockClient
+    SubClient = subBlockClient
 }
